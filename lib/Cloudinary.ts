5ff385a9842d8a2bbeee3a63d4aaf233fc78f571
muhhedahmd <asshxx1234567@gmail.com>
2026-02-03 00:19:57 +0200

    Fix environment variable references for Cloudinary configuration

--- lib/Cloudinary.ts
+++ lib/Cloudinary.ts
@@ -6,8 +6,8 @@ import sharp from "sharp";
 import { encode } from "blurhash";
 import fetch from "node-fetch";
 cloudinary.v2.config({
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY!,
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!  ,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
   api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
